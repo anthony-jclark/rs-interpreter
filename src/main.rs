814673b76ecaadecae2ffc1648c7aec721197e32
Anthony J. Clark <ajc@>
2017-03-02 17:50:45 -0600

    Finished Part 10.

--- src/main.rs
+++ src/main.rs
@@ -138,13 +138,14 @@ impl Lexer {
 
     fn get_next_token(&mut self) -> Option<Token> {
         self.skip_whitespace();
-        if self.done() { return None; }
 
         // Skip comments
         while let Some('{') = self.char() {
             self.skip_comment();
             self.skip_whitespace();
         }
+
+        if self.done() { return None; }
 
         let token = match self.char().unwrap() {
             '+' => Token::Plus,
@@ -172,44 +173,43 @@ impl Lexer {
 }
 
 
-/// Program : PROGRAM variable SEMI block DOT
-/// Block : declarations compound_statement
-/// Declarations : VAR (variable_declarations SEMI)+
+/// program : PROGRAM variable SEMI block DOT
+/// block : declarations compound_statement
+/// declarations : VAR (variable_declarations SEMI)+
 ///              | empty
-/// Variable_declarations : ID (COMMA ID)* COLON type_specifier
-/// Type_specifier : INTEGER | REAL
-/// Compound_statement : BEGIN statements END
-/// Statements : statement (SEMI statements)*
-/// Statement : compound_statement
+/// variable_declarations : ID (COMMA ID)* COLON type_specifier
+/// type_specifier : INTEGER | REAL
+/// compound_statement : BEGIN statements END
+/// statements : statement (SEMI statements)*
+/// statement : compound_statement
 ///           | assignment_statement
 ///           | empty
-/// Assignment_statement : variable ASSIGN expression
-/// Variable : ID
-/// Empty :
+/// assignment_statement : variable ASSIGN expression
+/// variable : ID
+/// empty :
 ///
-/// Expression : term ((PLUS | MINUS) term)*
-/// Term : factor ((STAR | DIV | SLASH) factor)*
-/// Factor : (PLUS | MINUS) factor
+/// expression : term ((PLUS | MINUS) term)*
+/// term : factor ((STAR | DIV | SLASH) factor)*
+/// factor : (PLUS | MINUS) factor
 ///        | INTEGERLITERAL
 ///        | REALLITERAL
 ///        | LPAREN expression RPAREN
 ///        | variable
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum AST {
-    Program { variable: Box<AST>, block: Box<AST> },
-    Block { declarations: Box<AST>, compound_statement: Box<AST> },
-    Declarations (Vec<Box<AST>>);
-    VariableDeclarations { variables: Vec<Box<AST>>, type_specifier: Box<AST> },
+    Program { name: Box<AST>, block: Box<AST> },
+    Block { declarations: Vec<Box<AST>>, compound_statement: Box<AST> },
+    VariableDeclaration { name: String, type_specifier: Box<AST> },
     TypeSpecifier (Token),
-    CompoundStatement (Box<AST>),
-    Statements (Vec<Box<AST>>)
-    Assign { lhs: Box<AST>, rhs: Box<AST> },
+    CompoundStatement (Vec<Box<AST>>),
+    AssignmentStatement { lhs: Box<AST>, rhs: Box<AST> },
     Variable (String),
-    NoOp,
-    BinOp { lhs: Box<AST>, op: Token, rhs: Box<AST> },
-    UnaryOp { op: Token, rhs: Box<AST> },
-    Num (i32),
+    Empty,
+    BinaryOperator { lhs: Box<AST>, op: Token, rhs: Box<AST> },
+    UnaryOperator { op: Token, rhs: Box<AST> },
+    Integer (i32),
+    Real (f64),
 }
 
 #[derive(Debug)]
@@ -243,12 +243,21 @@ impl Parser {
 
     /// program : PROGRAM variable SEMI block DOT
     fn program(&mut self) -> AST {
-        let ast = self.compound_statement();
+        match self.current_token {
+            Some(Token::Program) => self.advance(),
+            ref t => panic!("Error: expected a Program found a {:?}", t),
+        };
+        let name = self.variable();
+        match self.current_token {
+            Some(Token::Semi) => self.advance(),
+            ref t => panic!("Error: expected a Semi found a {:?}", t),
+        };
+        let block = self.block();
         match self.current_token {
             Some(Token::Dot) => self.advance(),
             ref t => panic!("Error: expected a Dot found a {:?}", t),
         };
-        ast
+        AST::Program{name: Box::new(name), block: Box::new(block)}
     }
 
     /// block : declarations compound_statement
@@ -267,7 +276,7 @@ impl Parser {
             Some(Token::Var) => {
                 self.advance();
                 while let Some(Token::Id(_)) = self.current_token {
-                    declarations.push(Box::new(self.variable_declaration()));
+                    declarations.extend(self.variable_declarations());
                     match self.current_token {
                         Some(Token::Semi) => self.advance(),
                         ref t => panic!("Error: expected a Semi found a {:?}", t),
@@ -280,13 +289,41 @@ impl Parser {
     }
 
     /// variable_declarations : ID (COMMA ID)* COLON type_specifier
-    fn variable_declarations(&mut self) -> AST {
-        unimplemented!();
+    fn variable_declarations(&mut self) -> Vec<Box<AST>> {
+        let mut ids: Vec<String> = Vec::new();
+        match self.current_token {
+            Some(Token::Id(ref s)) => ids.push(s.clone()),
+            ref t => panic!("Error: expected an Id found a {:?}", t),
+        }
+        self.advance();
+        while let Some(Token::Comma) = self.current_token {
+            self.advance();
+            match self.current_token {
+                Some(Token::Id(ref s)) => ids.push(s.clone()),
+                ref t => panic!("Error: expected an Id found a {:?}", t),
+            }
+            self.advance();
+        }
+        match self.current_token {
+            Some(Token::Colon) => self.advance(),
+            ref t => panic!("Error: expected a Colon found a {:?}", t),
+        }
+        let type_specifier = self.type_specifier();
+        ids.into_iter().map(|s| Box::new(AST::VariableDeclaration{
+            name: s,
+            type_specifier: Box::new(type_specifier.clone()),
+        })).collect()
     }
 
     /// type_specifier : INTEGER | REAL
     fn type_specifier(&mut self) -> AST {
-        unimplemented!();
+        match self.current_token {
+            // Some(Token::IntegerLiteral(int)) => { self.advance(); AST::Integer(int) }
+            // Some(Token::RealLiteral(real)) => { self.advance(); AST::Real(rea
+            Some(Token::Integer) => { self.advance(); AST::TypeSpecifier(Token::Integer) }
+            Some(Token::Real)    => { self.advance(); AST::TypeSpecifier(Token::Real) }
+            ref t => panic!("Error: expected a type_specifier found a {:?}", t),
+        }
     }
 
     /// compound_statement : BEGIN statements END
@@ -295,7 +332,7 @@ impl Parser {
             Some(Token::Begin) => self.advance(),
             ref t => panic!("Error: expected a Begin found a {:?}", t),
         };
-        let ast = AST::Compound(self.statement_list());
+        let ast = AST::CompoundStatement(self.statements());
         match self.current_token {
             Some(Token::End) => self.advance(),
             ref t => panic!("Error: expected a End found a {:?}", t),
@@ -336,7 +373,7 @@ impl Parser {
             Some(Token::Assign) => self.advance(),
             ref t => panic!("Error: expected an Assign found a {:?}", t),
         };
-        AST::Assign{lhs: Box::new(lhs), rhs: Box::new(self.expression())}
+        AST::AssignmentStatement{lhs: Box::new(lhs), rhs: Box::new(self.expression())}
     }
 
     /// variable : ID
@@ -351,7 +388,7 @@ impl Parser {
 
     /// empty :
     fn empty(&mut self) -> AST {
-        AST::NoOp
+        AST::Empty
     }
 
     /// expression : term ((PLUS | MINUS) term)*
@@ -366,7 +403,7 @@ impl Parser {
             }
             let token = self.current_token.clone().unwrap();
             self.advance();
-            ast = AST::BinOp {
+            ast = AST::BinaryOperator {
                 lhs: Box::new(ast), op: token, rhs: Box::new(self.term())
             };
         }
@@ -380,12 +417,13 @@ impl Parser {
         loop {
             match self.current_token {
                 Some(Token::Star)  => (),
-                Some(Token::Div) => (),
+                Some(Token::Div)   => (),
+                Some(Token::Slash) => (),
                 _ => break,
             }
             let token = self.current_token.clone().unwrap();
             self.advance();
-            ast = AST::BinOp {
+            ast = AST::BinaryOperator {
                 lhs: Box::new(ast), op: token, rhs: Box::new(self.factor())
             };
         }
@@ -402,12 +440,17 @@ impl Parser {
             Some(Token::Plus) | Some(Token::Minus) => {
                 let token = self.current_token.clone().unwrap();
                 self.advance();
-                AST::UnaryOp{op: token, rhs: Box::new(self.factor())}
+                AST::UnaryOperator{op: token, rhs: Box::new(self.factor())}
             }
 
             Some(Token::IntegerLiteral(i)) => {
                 self.advance();
-                AST::Num(i)
+                AST::Integer(i)
+            }
+
+            Some(Token::RealLiteral(r)) => {
+                self.advance();
+                AST::Real(r)
             }
 
             Some(Token::LParen) => {
@@ -440,36 +483,57 @@ impl Interpreter {
         }
     }
 
+    // Program { name: Box<AST>, block: Box<AST> },
+    // Block { declarations: Vec<Box<AST>>, compound_statement: Box<AST> },
+    // VariableDeclaration { name: String, type_specifier: Box<AST> },
+    // TypeSpecifier (Token),
+    // CompoundStatement (Vec<Box<AST>>),
+    // AssignmentStatement { lhs: Box<AST>, rhs: Box<AST> },
+    // Empty,
     fn visit(&mut self, ast: AST) {
         match ast {
-            AST::Compound(children) => {
+            AST::Program{name, block} => self.visit(*block),
+            AST::Block{declarations, compound_statement} => {
+                for vardec in declarations {
+                    self.visit(*vardec)
+                }
+                self.visit(*compound_statement);
+            }
+            AST::VariableDeclaration{name, type_specifier} => (),
+            AST::CompoundStatement(children) => {
                 for child in children {
                     self.visit(*child);
                 }
             }
-            AST::Assign{lhs, rhs} => match *lhs {
+            AST::AssignmentStatement{lhs, rhs} => match *lhs {
                 AST::Variable(s) => {
                     let val = self.evaluate(*rhs);
                     self.global_scope.insert(s, val);
                 }
                 ref t => panic!("Error: expected variable and found {:?}", t),
             },
-            AST::NoOp => (),
+            AST::Empty => (),
 
             ref t => panic!("Error: expected statement and found {:?}", t),
         }
     }
 
+    // BinaryOperator { lhs: Box<AST>, op: Token, rhs: Box<AST> },
+    // UnaryOperator { op: Token, rhs: Box<AST> },
+    // Variable (String),
+    // Integer (i32),
+    // Real (f64),
     fn evaluate(&self, ast: AST) -> i32 {
         match ast {
-            AST::BinOp{lhs, op, rhs} => match op {
+            AST::BinaryOperator{lhs, op, rhs} => match op {
                 Token::Plus  => self.evaluate(*lhs) + self.evaluate(*rhs),
                 Token::Minus => self.evaluate(*lhs) - self.evaluate(*rhs),
                 Token::Star  => self.evaluate(*lhs) * self.evaluate(*rhs),
                 Token::Div   => self.evaluate(*lhs) / self.evaluate(*rhs),
+                Token::Slash => self.evaluate(*lhs) / self.evaluate(*rhs),
                 _ => panic!("Error: expected arithmetic operator and found '{:?}'.", op),
             },
-            AST::UnaryOp{op, rhs} => match op {
+            AST::UnaryOperator{op, rhs} => match op {
                 Token::Plus  => self.evaluate(*rhs),
                 Token::Minus => -self.evaluate(*rhs),
                 _ => panic!("Error: expected a '+' or '-' and found '{:?}'.", op),
@@ -478,7 +542,8 @@ impl Interpreter {
                 Some(val) => *val,
                 None => panic!("Error: the variable {} is undefined", name),
             },
-            AST::Num(i) => i,
+            AST::Integer(i) => i,
+            AST::Real(r) => r as i32,
             ref t => panic!("Error: expected an evaluatable expression found {:?}", t),
         }
     }
@@ -497,7 +562,7 @@ fn main() {
     match args.len() {
         1 => run_prompt(),
         2 => run_file(&args[1]),
-        _ => println!("Usage: lox [script]")
+        _ => println!("Usage: rs_interpreter [script]")
     }
 }
 
